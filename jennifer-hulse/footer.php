e331b0e8c495d852d962d6e5bf9590526c76c722
aweidele <aweidele@gmail.com>
2017-06-16 14:27:32 -0400

    Add footer controls

--- jennifer-hulse/footer.php
+++ jennifer-hulse/footer.php
@@ -1,5 +1,6 @@
         <!--footer start 1-->
         <footer id="footer" class="dark">
+<?php /*
             <div class="primary-footer">
                 <div class="container">
                     <div class="row">
@@ -88,20 +89,30 @@
                     </div>
                 </div>
             </div>
+<?php */ 
 
+$social = array();
+if (get_option('theme_facebook') 	!= '')  $social['facebook'] = get_option('theme_facebook');
+if (get_option('theme_twitter') 	!= '') $social['twitter'] = get_option('theme_twitter');
+if (get_option('theme_instagram') 	!= '') $social['instagram'] = get_option('theme_instagram');
+if (get_option('theme_pinterest') 	!= '') $social['pinterest'] = get_option('theme_pinterest');
+if (get_option('theme_youtube') 	!= '') $social['youtube'] = get_option('theme_youtube');
+if (get_option('theme_houzz') 		!= '') $social['houzz'] = get_option('theme_houzz');
+if (get_option('theme_google') 		!= '') $social['google-plus'] = get_option('theme_google');
+if (get_option('theme_behance') 	!= '') $social['behance'] = get_option('theme_behance');
+if (get_option('theme_dribbble') 	!= '') $social['dribbble'] = get_option('theme_dribbble');
+?>
             <div class="secondary-footer">
                 <div class="container">
                     <div class="row">
                         <div class="col-md-6">
-                            <span class="m-top-10">Copyright 2012 - 2015 Massive Theme by <a href="http://themebucket.net/" class="f-link" target="_blank">ThemeBucket</a> | All Rights Reserved</span>
+                            <span class="m-top-10"><?php echo get_option('theme_copyright'); ?></span>
                         </div>
                         <div class="col-md-6">
                             <div class="social-link circle pull-right">
-                                <a href="#"><i class="fa fa-facebook"></i></a>
-                                <a href="#"><i class="fa fa-twitter"></i></a>
-                                <a href="#"><i class="fa fa-dribbble"></i></a>
-                                <a href="#"><i class="fa fa-google-plus"></i></a>
-                                <a href="#"><i class="fa fa-behance"></i></a>
+<?php foreach($social as $class => $link) { ?>
+                                <a href="<?php echo $link; ?>" target="_blank"><i class="fa fa-<?php echo $class; ?>"></i></a>
+<?php } ?>
                             </div>
                         </div>
                     </div>
--- jennifer-hulse/functions.php
+++ jennifer-hulse/functions.php
@@ -52,4 +52,82 @@ function register_my_menu() {
   register_nav_menu( 'primary-menu', __( 'Primary Menu' ) );
   //register_nav_menu( 'footer-menu', __( 'Footer Menu' ) );
 }
+
+
+
+/*** THEME CUSTOMIZATION SETTINGS ***/
+function mytheme_customize_register( $wp_customize ) {
+	
+	// ADD SECTION
+	$wp_customize->add_section( 'theme_footer' , array(
+    	'title'      => __( 'Footer Information', 'mytheme' ),
+		'priority'   => 30,) 
+  	);
+	
+	// ADD OPTIONS
+	$wp_customize->add_setting( 'theme_copyright'    , array('transport' => 'refresh','type' => 'option'));
+	$wp_customize->add_setting( 'theme_facebook'    , array('transport' => 'refresh','type' => 'option'));
+	$wp_customize->add_setting( 'theme_twitter'    , array('transport' => 'refresh','type' => 'option'));
+	$wp_customize->add_setting( 'theme_instagram'    , array('transport' => 'refresh','type' => 'option'));
+	$wp_customize->add_setting( 'theme_pinterest'    , array('transport' => 'refresh','type' => 'option'));
+	$wp_customize->add_setting( 'theme_youtube'    , array('transport' => 'refresh','type' => 'option'));
+	$wp_customize->add_setting( 'theme_houzz'    , array('transport' => 'refresh','type' => 'option'));
+	$wp_customize->add_setting( 'theme_google'    , array('transport' => 'refresh','type' => 'option'));
+	$wp_customize->add_setting( 'theme_dribbble'    , array('transport' => 'refresh','type' => 'option'));
+	$wp_customize->add_setting( 'theme_behance'    , array('transport' => 'refresh','type' => 'option'));
+	
+	/// ADD CONTROLS
+	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'theme_copyright', array(
+		'label'        => __( 'Copyright info', 'mytheme' ),
+		'section'    => 'theme_footer',
+		'settings'   => 'theme_copyright',
+  	) ) );
+	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'theme_facebook', array(
+		'label'        => __( 'Facebook', 'mytheme' ),
+		'section'    => 'theme_footer',
+		'settings'   => 'theme_facebook',
+  	) ) );
+	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'theme_twitter', array(
+		'label'        => __( 'Twitter', 'mytheme' ),
+		'section'    => 'theme_footer',
+		'settings'   => 'theme_twitter',
+  	) ) );
+	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'theme_instagram', array(
+		'label'        => __( 'Instagram', 'mytheme' ),
+		'section'    => 'theme_footer',
+		'settings'   => 'theme_instagram',
+  	) ) );
+	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'theme_pinterest', array(
+		'label'        => __( 'Pinterest', 'mytheme' ),
+		'section'    => 'theme_footer',
+		'settings'   => 'theme_pinterest',
+  	) ) );
+	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'theme_youtube', array(
+		'label'        => __( 'YouTube', 'mytheme' ),
+		'section'    => 'theme_footer',
+		'settings'   => 'theme_youtube',
+  	) ) );
+	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'theme_houzz', array(
+		'label'        => __( 'Houzz', 'mytheme' ),
+		'section'    => 'theme_footer',
+		'settings'   => 'theme_houzz',
+  	) ) );
+	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'theme_google', array(
+		'label'        => __( 'Google+', 'mytheme' ),
+		'section'    => 'theme_footer',
+		'settings'   => 'theme_google',
+  	) ) );
+	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'theme_dribbble', array(
+		'label'        => __( 'Dribbble', 'mytheme' ),
+		'section'    => 'theme_footer',
+		'settings'   => 'theme_dribbble',
+  	) ) );
+	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'theme_behance', array(
+		'label'        => __( 'Behance', 'mytheme' ),
+		'section'    => 'theme_footer',
+		'settings'   => 'theme_behance',
+  	) ) );
+}
+
+add_action( 'customize_register', 'mytheme_customize_register' );
 ?>
