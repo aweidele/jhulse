3d3e593f9fab9cb1e48b935dd8c169942996e70d
Aaron Weidele <aweidele@gmail.com>
2017-08-04 14:48:14 -0600

    Display the correct archive title

--- jennifer-hulse/archive-portfolio.php
+++ jennifer-hulse/archive-portfolio.php
@@ -4,7 +4,7 @@
 					<div class="container">
 							<div class="row">
 									<div class="col-md-12">
-											<h2><?php the_archive_title(); ?></h2>
+											<h2><?php echo str_replace( 'Archives: ','',get_the_archive_title() ); ?></h2>
 											<!-- ol class="breadcrumb">
 													<li><a href="#">Home</a>
 													</li>
