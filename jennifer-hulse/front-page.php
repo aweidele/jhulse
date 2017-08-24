398e2b00bac9527e80a53443e24d0b57b5dff426
Aaron Weidele <aweidele@gmail.com>
2017-08-24 12:34:37 -0400

    reset

--- jennifer-hulse/front-page.php
+++ jennifer-hulse/front-page.php
@@ -35,8 +35,6 @@
 							$terms = wp_get_post_terms($post->ID,'project_category');
 							if(sizeof($gallery)) {
 ?>
-
-<pre><?php print_r($post); ?></pre>
                             <div class="portfolio-item<?php foreach($terms as $cat) { echo " cat-".$cat->slug; } ?>">
                                 <div class="thumb">
 <?php if(sizeof($gallery) == 1) { ?>
@@ -45,8 +43,9 @@
                                     <div class="portfolio-slider">
                                         <ul class="slides">
                                            <?php foreach($gallery as $image) { ?>
+                                           
                                             <li>
-                                                <a href="<?php echo $image['url']; ?>" title="<?php //echo $post->title">
+                                                <a href="<?php echo $image['url']; ?>" title="Freshness Photo">
                                                     <img src="<?php echo $image['sizes']['Homepage Portfolio']; ?>" alt="">
                                                 </a>
                                             </li>
